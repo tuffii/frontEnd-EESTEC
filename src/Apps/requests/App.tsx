db3b36c3efea2c456f32802cddd235f76fa0a539
_tuffi_ <syrbu.gv@edu.spbstu.ru>
2023-04-07 20:07:32 +0300

    fix

--- src/Apps/requests/App.tsx
+++ src/Apps/requests/App.tsx
@@ -208,6 +208,177 @@ const DefaultUsers: Components.userList.Users = [
     Phone: '+79113322335',
     Flag: false,
   },
+  {
+    Name: {
+      First: 'Sokolova',
+      Second: 'Nataliya',
+      Third: 'Sergeevna',
+    },
+    Phone: '+79113322337',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Vasiliev',
+      Second: 'Andrey',
+      Third: 'Vladimirovich',
+    },
+    Phone: '+79113322338',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Sergeeva',
+      Second: 'Maria',
+      Third: 'Ivanovna',
+    },
+    Phone: '+79113322339',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Kuznetsova',
+      Second: 'Ekaterina',
+      Third: 'Vladimirovna',
+    },
+    Phone: '+79113322330',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Ivanova',
+      Second: 'Anna',
+      Third: 'Sergeevna',
+    },
+    Phone: '+79113322331',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Smirnova',
+      Second: 'Tatiana',
+      Third: 'Vladimirovna',
+    },
+    Phone: '+79113322332',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Popova',
+      Second: 'Elena',
+      Third: 'Sergeevna',
+    },
+    Phone: '+79113322333',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Morozova',
+      Second: 'Svetlana',
+      Third: 'Vladimirovna',
+    },
+    Phone: '+79113322334',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Vladimirov',
+      Second: 'Dmitry',
+      Third: 'Sergeevich',
+    },
+    Phone: '+79113322335',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Kharlamova',
+      Second: 'Nadezhda',
+      Third: 'Vladimirovna',
+    },
+    Phone: '+79113322336',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Zhukova',
+      Second: 'Lyubov',
+      Third: 'Sergeevna',
+    },
+    Phone: '+79113322337',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Bakhtin',
+      Second: 'Sergey',
+      Third: 'Vladimirovich',
+    },
+    Phone: '+79113322338',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Novikova',
+      Second: 'Tatiana',
+      Third: 'Sergeevna',
+    },
+    Phone: '+79113322339',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Vasilieva',
+      Second: 'Galina',
+      Third: 'Vladimirovna',
+    },
+    Phone: '+79113322330',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Sokolov',
+      Second: 'Dmitry',
+      Third: 'Sergeevich',
+    },
+    Phone: '+79113322331',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Petrov',
+      Second: 'Ivan',
+      Third: 'Sergeevich',
+    },
+    Phone: '+79113322332',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Ivanova',
+      Second: 'Nataliya',
+      Third: 'Sergeevna',
+    },
+    Phone: '+79113322333',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Morozov',
+      Second: 'Andrey',
+      Third: 'Sergeevich',
+    },
+    Phone: '+79113322334',
+    Flag: false,
+  },
+  {
+    Name: {
+      First: 'Sergeev',
+      Second: 'Vladimir',
+      Third: 'Sergeevich',
+    },
+    Phone: '+79113322335',
+    Flag: false,
+  },
 ]
 
 const Component: SolidJS.Component = () => {
@@ -225,64 +396,64 @@ const Component: SolidJS.Component = () => {
   const Element: SolidJS.JSX.Element = (
     <div class={`customElement`} id={`requests`}>
 
-      {(rejectInvite() && GetCheckedUsers().length != 0) && (
+
+      <SolidJS.Show when={acceptInvite()}>
         <div class={'blacker'}>
-          <div class={'meny'}>
-            <div class={'head'}>
-              <div class={'textContainer'}>
-                <div class={'text'}>
-                  Принять эти заявки
-                </div>
+        <div class={'meny'}>
+          <div class={'head'}>
+            <div class={'textContainer'}>
+              <div class={'text'}>
+                Принять эти заявки
               </div>
-              <button
-                class={'close'}
-                onClick={(_) => {
-                  setRejectInvite(!rejectInvite())
-                }}>
-                <i class='fa-light fa-xmark fa-2x'></i>
-              </button>
             </div>
-            <div class={'settings'}>
-              <Components.flagedusers.default List={GetCheckedUsers()} />
-            </div>
-            <div class={'footer'}>
-            <button onClick={(_) => {
-                  setRejectInvite(!rejectInvite())
-                }}> подтвердить</button>
-            </div>
+            <button
+              class={'close'}
+              onClick={(_) => {
+                setAcceptInvite(!acceptInvite())
+              }}>
+              <i class='fa-light fa-xmark fa-2x'></i>
+            </button>
+          </div>
+          <div class={'settings'}>
+            <Components.flagedusers.default List={GetCheckedUsers()} />
+          </div>
+          <div class={'footer'}>
+          <button onClick={(_) => {
+                setAcceptInvite(!acceptInvite())
+              }}> подтвердить</button>
           </div>
         </div>
-      )}
+      </div>
+    </SolidJS.Show>
 
-      {acceptInvite() && (
+    <SolidJS.Show when={rejectInvite()}>
         <div class={'blacker'}>
-          <div class={'meny'}>
-            <div class={'head'}>
-              <div class={'textContainer'}>
-                <div class={'text'}>
-                  Отклонить эти заявки
-                </div>
+        <div class={'meny'}>
+          <div class={'head'}>
+            <div class={'textContainer'}>
+              <div class={'text'}>
+                Отклонить эти заявки
               </div>
-              <button
-                class={'close'}
-                onClick={(_) => {
-                  setAcceptInvite(!acceptInvite())
-                }}>
-                <i class='fa-light fa-xmark fa-2x'></i>
-              </button>
             </div>
-            <div class={'settings'}>
-              <Components.flagedusers.default List={GetCheckedUsers()} />
-            </div>
-            <div class={'footer'}>
-              <button onClick={(_) => {
-                  setAcceptInvite(!acceptInvite())
-                }}> подтвердить</button>
-            </div>
+            <button
+              class={'close'}
+              onClick={(_) => {
+                setRejectInvite(!rejectInvite())
+              }}>
+              <i class='fa-light fa-xmark fa-2x'></i>
+            </button>
+          </div>
+          <div class={'settings'}>
+            <Components.flagedusers.default List={GetCheckedUsers()} />
+          </div>
+          <div class={'footer'}>
+          <button onClick={(_) => {
+                setRejectInvite(!rejectInvite())
+              }}> подтвердить</button>
           </div>
         </div>
-      )}
-
+      </div>
+    </SolidJS.Show>
       <div class={'header'}>
         <div class={'search'}>
           <div class={'glass'}>
@@ -299,7 +470,7 @@ const Component: SolidJS.Component = () => {
         <button
           class={'multiple'}
           onClick={(_) => {
-            setAcceptInvite(!acceptInvite())
+            setRejectInvite(!rejectInvite() && GetCheckedUsers().length != 0)
           }}>
           <i class='fa-light fa-square-xmark fa-3x' style={'color: #666666'}></i>
         </button>
@@ -307,7 +478,7 @@ const Component: SolidJS.Component = () => {
         <button
           class={'multiple'}
           onClick={(_) => {
-            setRejectInvite(!rejectInvite())
+            setAcceptInvite(!acceptInvite() && GetCheckedUsers().length != 0)
           }}>
           <i class='fa-light fa-square-check fa-3x' style={'color: #666666'}></i>
         </button>
